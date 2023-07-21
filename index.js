c423e87e6e6ff888428d9b910951f416421f3421
Navin-kumar2502 <114897504+Navin-kumar2502@users.noreply.github.com>
2023-07-21 21:32:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const http=require('http');
-const fs=require('fs');
-var requests = require('requests');
-const homeFile=fs.readFileSync(index.html,"UTF-8");
-const replaceVal=(tempVal,orgVal)=>{
+const express = require('express');
+const requests = require('requests');
+const fs = require('fs');
+const app = express();
+const port = 8000;
+const homeFile = fs.readFileSync('index.html', 'UTF-8');
+const replaceVal = (tempVal, orgVal) => {
     let tempe=tempVal.replace("{%tempval%}",orgVal.main.temp);
     tempe=tempe.replace("{%tempmin%}",orgVal.main.temp_min);
     tempe=tempe.replace("{%tempmax%}",orgVal.main.temp_max);
@@ -12,23 +14,32 @@ const replaceVal=(tempVal,orgVal)=>{
     tempe=tempe.replace("{%tempstatus%}",orgVal.weather[0].main);
     return tempe;
 };
-const server = http.createServer((req,res)=>{
-    if(req.url=="/"){
-        //copy pasted
-        requests("https://api.openweathermap.org/data/2.5/weather?q=surat&units=metric&appid=cbac6b09413a5ade3642f3e727740c3b")
-        .on("data",(chunk)=>{
-            const objdata=JSON.parse(chunk);
-            const arrData=[objdata];
-            const realTimeData=arrData
-            .map((val)=>replaceVal(homeFile,val)).join("");
-            res.write(realTimeData);
-            //console.log(realTimeData); 
+const displayError = (res, errorMessage) => {
+    const errorHTML = `<div style="text-align: center; margin-top: 50px;">
+        <h2>${errorMessage}</h2>
+        <a href="/">Go back to home</a>
+    </div>`;
+    res.send(errorHTML);
+};
+app.get('/', (req, res) => {
+    const searchCity = req.query.city || 'Mumbai';
+    requests(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&units=metric&appid=cbac6b09413a5ade3642f3e727740c3b`)
+        .on('data', (chunk) => {
+            const objdata = JSON.parse(chunk);
+            if (objdata.cod === '404') {
+                displayError(res, 'City not found. Please enter a valid city name.');
+            } else {
+                const arrData = [objdata];
+                const realTimeData = arrData.map((val) => replaceVal(homeFile, val)).join('');
+                res.send(realTimeData); 
+            }
         })
-        .on("end",(err)=>{
-            if(err) return console.log("connection error",err);
-            res.end();
+        .on('error', (err) => {
+            console.error('Error:', err.message);
+            displayError(res, 'Internal Server Error. Please try again later.');
         });
-    }
-}); 
-
-server.listen(8000,"127.0.0.1");             
+});
+app.use(express.static('public'));
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
